57522a441ab0c1456ee90c1282c855763ce9b095
Cesar Augusto Nogueira <cesarnogueira1210@gmail.com>
2017-07-31 12:10:37 -0300

    Fix travis.yml

--- server/test_travis.js
+++ server/test_travis.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n') // this will FAIL travis ci lint
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+  res.end('Hello Travis!\n'); // build should pass now!
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
